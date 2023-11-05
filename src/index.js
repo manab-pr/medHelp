1b5ff529f6f637906a5cc9fcd90781aaecb893e7
Manab Pratim Maity <manab2001maity@gmail.com>
2023-11-05 20:15:20 +0530

    Currently it can create QR code using url.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const telegramBot=require('node-telegram-bot-api');
+const QR = require('qrcode');
+const fs = require('fs') 
 
 require('dotenv').config();
 
@@ -10,20 +12,36 @@ const bot= new telegramBot(TOKEN,{polling: true})
 
 bot.on('message',(message)=>{
    let chatId =message.from.id;
-   const photopath='.\Images\pexels-dat-vu-mm-17986565.jpg'
+   const Text =message.text;
+   console.log(chatId);
+
+   if(isvalidUrl(Text)){
+      QR.toFile('foo.png',Text,(err)=>{
+         if(err){
+            console.error('Error generating Qr code',err);
+         }else{
+            bot.sendPhoto(chatId,'foo.png',{caption:'Here is your Qr code',contentType:'image/png'})
+            .then(()=>{console.log('Qr code send successfully');
+            fs.unlinkSync('foo.png')
+         })
+           .catch((error)=>{
+            console.error('Error Sending QR code',error);
+
+         })
+
+         }
+
+      }
+
+   )  
+      
+   }
    
-   if(message.text.toLowerCase()=="hey"){
-    bot.sendMessage(chatId,'What is your name?')
-   }
-   else{
-    bot.sendMessage(chatId,`Welcome to Med Help ${message.text}`)
-
-   }
-
-  
+   
 })
 
-// bot.onText(/\/Hello/,(msg)=>{
-//    const chatId=msg.from.id;
-//    bot.sendMessage(chatId,'Hey,I am here to assist you')
-// })
+const isvalidUrl=(string)=>{
+   const urlRegx= /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/ ;
+   return urlRegx.test(string);
+}
+
